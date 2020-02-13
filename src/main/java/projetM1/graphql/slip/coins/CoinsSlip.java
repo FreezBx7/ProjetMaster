40c7281488aced5da8f9d429c56f4aefac9ff2d4
Cyriaque <cyriaque.hockey@hotmail.fr>
2020-02-13 09:37:30 +0100

    feat:addVariableGetterAndConstruct

--- src/main/java/projetM1/graphql/slip/coins/CoinsSlip.java
+++ src/main/java/projetM1/graphql/slip/coins/CoinsSlip.java
@@ -1,5 +1,61 @@
 package projetM1.graphql.slip.coins;
 
-public class CoinsSlip {
+import projetM1.graphql.slip.Slip;
+
+public class CoinsSlip extends Slip {
+	
+	private final int two;
+	private final int one;
+	private final int fiftycents;
+	private final int twentycents;
+	private final int tencents;
+	private final int fivecents;
+	private final int twocents;
+	private final int onecents;
+	
+	
+	
+	
+	public CoinsSlip(String date, double total_amount, int memberId, String num_slip, int two, int one, int fiftycents,
+			int twentycents, int tencents, int fivecents, int twocents, int onecents) {
+		super(date, total_amount, memberId, num_slip);
+		this.two = two;
+		this.one = one;
+		this.fiftycents = fiftycents;
+		this.twentycents = twentycents;
+		this.tencents = tencents;
+		this.fivecents = fivecents;
+		this.twocents = twocents;
+		this.onecents = onecents;
+	}
+	
+	
+	public int getTwo() {
+		return two;
+	}
+	public int getOne() {
+		return one;
+	}
+	public int getFiftycents() {
+		return fiftycents;
+	}
+	public int getTwentycents() {
+		return twentycents;
+	}
+	public int getTencents() {
+		return tencents;
+	}
+	public int getFivecents() {
+		return fivecents;
+	}
+	public int getTwocents() {
+		return twocents;
+	}
+	public int getOnecents() {
+		return onecents;
+	}
+	
+	
+	
 
 }
--- src/main/java/projetM1/graphql/slip/ticket/TicketSlip.java
+++ src/main/java/projetM1/graphql/slip/ticket/TicketSlip.java
@@ -1,6 +1,38 @@
 package projetM1.graphql.slip.ticket;
 
-public class TicketSlip {
+import projetM1.graphql.slip.Slip;
+
+public class TicketSlip extends Slip {
+
 	private final int ticketSlipId;
+	private final int fifty;
+	private final int twenty;
+	private final int ten;
+	private final int five;
+	public TicketSlip(String date, double total_amount, int memberId, String num_slip, int ticketSlipId, int fifty,
+			int twenty, int ten, int five) {
+		super(date, total_amount, memberId, num_slip);
+		this.ticketSlipId = ticketSlipId;
+		this.fifty = fifty;
+		this.twenty = twenty;
+		this.ten = ten;
+		this.five = five;
+	}
+	public int getTicketSlipId() {
+		return ticketSlipId;
+	}
+	public int getFifty() {
+		return fifty;
+	}
+	public int getTwenty() {
+		return twenty;
+	}
+	public int getTen() {
+		return ten;
+	}
+	public int getFive() {
+		return five;
+	}
+	
 	
 }
