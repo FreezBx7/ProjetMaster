26a6fe4154a3e528b4b5b95aef7097682e8f798e
Cyriaque <cyriaque.hockey@hotmail.fr>
2020-04-10 15:42:48 +0200

    fix(MemberFirstName)

--- src/main/java/projetM1/graphql/member/MemberRepository.java
+++ src/main/java/projetM1/graphql/member/MemberRepository.java
@@ -47,7 +47,7 @@ public class MemberRepository {
 		doc.append("id_member", member.getId_member());
         doc.append("card", member.getCard());
         doc.append("name", member.getName());
-        doc.append("first_name", member.getFirstName());
+        doc.append("firstName", member.getFirstName());
         doc.append("link_photo", member.getLink_photo());
         doc.append("email", member.getEmail());
         doc.append("price", member.getPriceId());
@@ -79,7 +79,7 @@ public class MemberRepository {
         return new Member(doc.getInteger("id_member"),
         				  doc.getString("card"),
         				  doc.getString("name"),
-        				  doc.getString("first_name"),
+        				  doc.getString("firstName"),
         				  doc.getString("link_photo"),
         				  doc.getString("email"),
         				  doc.getInteger("price"),
